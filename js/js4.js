5dcb1ae25164e474634df11bfdb5cd179aabd98d
Rocio <rociohernandez2067@gmail.com>
2022-10-28 13:21:19 -0500

    Se agregaron  nuevas lineas de codigo en js4

--- js/js4.js
+++ js/js4.js
@@ -5,3 +5,18 @@ frecuencia = Number(prompt("Ingrese la frecuencia (1-12):"))
 
 document.write("Hora incial: ", horaInicial)
 document.write("<br/>Frecuencia: ", frecuencia)
+
+let cantidad = Math.floor(24/frecuencia);
+
+for(i = 1;i<=cantidad;i++){
+
+    if(horaInicial>23){
+
+        horaInicial = horaInicial-24
+
+    }
+    console.log(`Toma ${i}: ${horaInicial}`)
+
+    horaInicial = horaInicial + frecuencia
+    document.write(`<h2> ${horaInicial} </h2>`);
+}
